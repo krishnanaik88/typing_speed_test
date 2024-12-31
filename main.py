c88a011f9c0453ed7d66a658b73bf6cb508ebde3
AnuragWakchaure <151008646+AnuragWakchaure@users.noreply.github.com>
2024-12-31 23:51:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+from TypeApp import TypeApp
+
+if __name__ == "__main__":
+    app = TypeApp.TypingApp()
+    app.run()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+import time
+from TypeApp import TypeApp
+
+
+
+# Run the application
+if __name__ == "__main__":
+    app = TypeApp.TypingApp()
+    app.run()
